8da45a0e319b8245e25b21e16efc32a6bdbf2aee
Summer <790961987@qq.com>
2017-11-17 22:34:54 +0800

    Create models.rs

---
+++ src/models.rs
@@ -0,0 +1,18 @@
+use diesel::prelude::*;
+
+use super::schema::blogs;
+#[derive(Queryable, Serialize, Debug, Deserialize)]
+pub struct Blog {
+    pub id: i64,
+    pub title: String,
+    pub content: String,
+    // pub created_time: DateTime<UTC>,
+}
+
+#[derive(Insertable, Debug, Serialize, Deserialize)]
+#[table_name="blogs"]
+pub struct NewBlog<'a> {
+    pub title: &'a str,
+    pub content: &'a str,
+    // pub created_time: DateTime<UTC>,
+}
